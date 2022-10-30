ddc420ffb4d87e8d97b929bac2559992361cda1d
Evgeniy <evgenii.z.i@yandex.ru>
2022-10-30 18:21:25 +0300

    Init Project

---
+++ wp-content/themes/twentytwentytwo/functions.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Twenty Twenty-Two functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Two
+ * @since Twenty Twenty-Two 1.0
+ */
+
+
+if ( ! function_exists( 'twentytwentytwo_support' ) ) :
+
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * @since Twenty Twenty-Two 1.0
+	 *
+	 * @return void
+	 */
+	function twentytwentytwo_support() {
+
+		// Add support for block styles.
+		add_theme_support( 'wp-block-styles' );
+
+		// Enqueue editor styles.
+		add_editor_style( 'style.css' );
+
+	}
+
+endif;
+
+add_action( 'after_setup_theme', 'twentytwentytwo_support' );
+
+if ( ! function_exists( 'twentytwentytwo_styles' ) ) :
+
+	/**
+	 * Enqueue styles.
+	 *
+	 * @since Twenty Twenty-Two 1.0
+	 *
+	 * @return void
+	 */
+	function twentytwentytwo_styles() {
+		// Register theme stylesheet.
+		$theme_version = wp_get_theme()->get( 'Version' );
+
+		$version_string = is_string( $theme_version ) ? $theme_version : false;
+		wp_register_style(
+			'twentytwentytwo-style',
+			get_template_directory_uri() . '/style.css',
+			array(),
+			$version_string
+		);
+
+		// Enqueue theme stylesheet.
+		wp_enqueue_style( 'twentytwentytwo-style' );
+
+	}
+
+endif;
+
+
+add_action( 'wp_enqueue_scripts', 'twentytwentytwo_styles' );
+
+// Add block patterns
+require get_template_directory() . '/inc/block-patterns.php';
+
+
+function  backuratk_endpoint( $request_data ) {
+    $args = array(
+        'post_type' => 'post',
+        'posts_per_page'=>-1, 
+        'numberposts'=>-1
+    );
+    
+    $posts = get_posts($args);
+    foreach ($posts as $key => $post) {
+        $posts[$key]->acf = get_fields($post->ID);
+    }
+
+    return  $posts;
+}
+    
+add_action('rest_api_init', function() {
+    register_rest_route( 'backuratk/v1', '/posts/', array(
+        'methods' => 'GET',
+        'callback' => 'backuratk_endpoint'
+    ));
+});
